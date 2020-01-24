134dde193d2114ffa645e355345e7164676df71b
blueCycle <agrawal.amitkumar@gmail.com>
2020-01-23 16:06:08 -0800

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World - Trying to understand this build"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
